7698f7993d773de34d4738069ef0642250adaefa
Antonio Rafael <antonio.robles6253@alumnos.udg.mx>
2025-10-07 10:26:58 -0600

    feat (apuntes/practicas) Agregar ejercicios de fgets,gets,fflush y put

---
+++ 2semPE/c/apuntes/practicas/00. Useful Functions/01-puts-strlen/src/respuesta gets.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LENGHT 81
+
+static void
+remove_newline (char *string)
+{
+  if (!string)
+    return;
+
+  size_t lenghtString = strlen (string); // usamos strlen no llena el string
+                                         // con un '\n' a diferencia de sizeof
+  if (lenghtString > 0 && string[lenghtString - 1] == '\n')
+    string[lenghtString - 1] = '\0';
+}
+
+static void
+printfCaraters (char *buffer)
+{
+  for (size_t i = 0; i <= strlen (buffer); i++)
+    {
+      printf ("Caracter: %c, Valor: %d\n", buffer[i], buffer[i]);
+    }
+}
+
+int
+main ()
+{
+  char buffer[MAX_LENGHT];
+
+  if (!gets (buffer) )// Si fgets esta vacio definimos el buffer como vacio
+    {
+      buffer[0] = '\0';
+    }
+  fflush(stdin);
+  remove_newline (buffer);
+
+  /* Log de consola*/
+  puts (buffer);
+  printf ("Longitud: %zu\n", strlen (buffer));
+  printfCaraters (buffer);
+
+  return 0;
+}
